d1981392372095971e56905d1448f633ac6b308e
Vlad Chira <chiravlad.andrei1234@gmail.com>
2025-05-09 18:00:36 +0300

    added basic register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,20 @@
+import { MessageCircle } from "lucide-react"
+
+import { RegisterForm } from "@/components/register-form"
+import { appName } from "@/lib/constants"
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <MessageCircle className="size-4" />
+          </div>
+          {appName}
+        </a>
+        <RegisterForm />
+      </div>
+    </div>
+  )
+}
