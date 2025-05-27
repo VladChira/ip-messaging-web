e414ab2288742b52aef6b78fbc6b2174859a58f9
Vlad Chira <chiravlad.andrei1234@gmail.com>
2025-05-27 22:23:36 +0300

    pwa support

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: false,
+  skipWaiting: true,
+  // buildExcludes: [/middleware-manifest\.json$/], // if you hit App Router issues
+});
+
 const nextConfig: NextConfig = {
   basePath: '/messaging',
   trailingSlash: true
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
-import type { Metadata } from "next";
+"use client"
+
+// import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import AuthWrapper from "@/components/AuthWrapper";
 import { Toaster } from "sonner"; // Import the Toaster directly from sonner
+import { useEffect } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,18 +18,37 @@ const geistMono = Geist_Mono({
   subsets: ["latin"], // Fixed typo 'suihbsets' -> 'subsets'
 });
 
-export const metadata: Metadata = {
-  title: "Messaging App",
-  description: "A great messaging app created for IP",
-};
+// export const metadata: Metadata = {
+//   title: "Messaging App",
+//   description: "A great messaging app created for IP",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/messaging/sw.js")
+        .then(() => console.log("SW registered"))
+        .catch(console.error);
+    }
+  }, []);
+
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/messaging/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+        {/* iOS splash/login: */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/messaging/icons/icon-192.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
